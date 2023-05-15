d334a252dd8dc810c97d60f7619c4c8cffb16ebc
Yehonatan Lachman <55838866+YehonatanLA@users.noreply.github.com>
2023-05-15 17:40:36 +0300

    main.py

--- Improvement/main.py
+++ Improvement/main.py
@@ -10,7 +10,6 @@ def run_algorithm(sections_num, letters_amount, real_edge_len, frequency, strand
     # declassify each read by its section
     four_pow = create_convert_list(read_size)
     try:
-        # classifications = create_longest_classifications(letters_amount, sections_num)
         longest_classifications = find_longest(letters_amount)
         classifications = longest_classifications[0: sections_num]
         paddings_hash = create_paddings_hash(classifications, padding_size)
@@ -23,22 +22,16 @@ def run_algorithm(sections_num, letters_amount, real_edge_len, frequency, strand
     except ValueError:
         print("Can't create this many sections with only this amount of letters")
         exit(0)
-    # TODO: add documentation
     reads_by_sections, max_splits_arr = declassify_reads(read_lst, frequency, letters_amount, classifications,
                                                          padding_size, paddings_hash, four_pow,
                                                          paddings_to_classifications, sections_num)
 
-    # run for each section Alex's algorithm
-    # TODO: currently assuming that the strand_len / sections_num is a whole number,
-    # change algorithm for unequal section lengths?
     strand_section_len_before = strand_len / sections_num
     special_section_length_no_padding = get_section_size(strand_section_len_before, frequency, letters_amount)
     complete_sections = run_parallel_algorithm(reads_by_sections, read_size, real_edge_len,
                                                special_section_length_no_padding, max_splits_arr)
 
-    # for sec in complete_sections:
-    #     print(sec)
-    #     print("\n\n\n")
+   
     if complete_sections is None:
         print("None, failed")
         return None
@@ -46,6 +39,5 @@ def run_algorithm(sections_num, letters_amount, real_edge_len, frequency, strand
 
     strand_rebuilt = remove_meta_data(sections_num, complete_sections, frequency, letters_amount, read_size,
                                       max_splits_arr)
-    # print(special_section_length_no_padding)
-    # print(strand_rebuilt)
+
     return strand_rebuilt
